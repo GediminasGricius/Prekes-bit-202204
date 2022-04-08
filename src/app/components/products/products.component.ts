b6362840908ccf22079ff350cb6678a4fe2308b3
GediminasGricius <g.gricius@ituostas.lt>
2022-04-08 21:15:42 +0300

    Paspaudimai

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -9,9 +9,25 @@ export class ProductsComponent implements OnInit {
 
   public vardai=["Jonas", "Antanas", "Kestas"];
 
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  public addNewVardas(vardas:HTMLInputElement){
+    if (vardas.value!=''){
+      this.vardai.push(vardas.value);
+      vardas.value='';
+    }
+  }
+
+  public removeVardas(i:number){
+    this.vardai.splice(i, 1);
+  }
+
+  
+  
+
 
 }
